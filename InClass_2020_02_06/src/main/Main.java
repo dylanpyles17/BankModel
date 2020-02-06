cef2519bba8f1fe3ae65edb0fa0e4b49369b4ee2
dylanpyles <dylanpyles@dylans-mbp.uccc.uc.edu>
2020-02-06 11:57:57 -0500

    I added code into the main class.

--- InClass_2020_02_06/src/main/Main.java
+++ InClass_2020_02_06/src/main/Main.java
@@ -1,9 +1,36 @@
 package main;
 
+import bank.Bank;
+import bank.BrickAndMortar;
+import bank.CafeBranch;
+import bank.Online;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Bank myBank = new Bank();
 		
+		myBank.setCompanyName("Fifth Third Bank");
+		
+		BrickAndMortar physicalBank = new BrickAndMortar();
+		
+		physicalBank.setCompanyName("Wells Fargo");
+		physicalBank.setAddress("1234 Bank Ave.");
+		
+		Online onlineBank = new Online();
+		
+		onlineBank.setCompanyName("PNC");
+		onlineBank.setUrl("https://www.pnc.com/en/personal-banking.html");
+		
+		CafeBranch starbucks = new CafeBranch();
+		
+		starbucks.setCompanyName("Starbucks");
+		starbucks.setMenu("Mocha - hot/cold");
+		
+		System.out.println(myBank.toString() + "/n");
+		System.out.println(physicalBank.toString() + "/n");
+		System.out.println(onlineBank.toString() + "/n");
+		System.out.println(starbucks.toString());
 	}
 
 }
